0c36369d423e518db459bdf047ffff7ea55b47ab
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2022-03-06 17:19:46 +1100

    Test pre-config

--- slice_of_life/audio/audio_handler.py
+++ slice_of_life/audio/audio_handler.py
@@ -1,22 +1,22 @@
+import os
 from audio.audio_file import AudioFile
 from ffmpeg_progress_yield import FfmpegProgress
-import os
 from mutagen.easyid3 import EasyID3
 
 
 class AudioHandler:
-    """ A class responsible for handling and processing multiple audio files. """
+    """A class responsible for handling and processing multiple audio files."""
 
     def bulk_split_audio_files(self, audio_files: list[AudioFile], destination_path: str) -> list[AudioFile]:
 
         split_audio_files = []
-        split_path = os.path.join(destination_path, 'tmp_split_audio__')
+        split_path = os.path.join(destination_path, "tmp_split_audio__")
 
         if not os.path.exists(split_path):
             os.mkdir(split_path)
 
         for audio_file in audio_files:
-            episode_sub_path = os.path.join(split_path, f'episode__{audio_file.title}')
+            episode_sub_path = os.path.join(split_path, f"episode__{audio_file.title}")
 
             if not os.path.exists(episode_sub_path):
                 os.mkdir(episode_sub_path)
@@ -29,22 +29,26 @@ class AudioHandler:
                         AudioFile(
                             title=file_name,
                             extension=file_ext,
-                            absolute_path=f'{episode_sub_path}/{f}',
+                            absolute_path=f"{episode_sub_path}/{f}",
                         )
                     )
 
         return split_audio_files
 
-    def split_audio(self, audio_file: AudioFile, segment_time: int = 180, output_dir: str = '/tmp') -> bool:
-        """ Splits an audio track at intervals determined by the supplied segment time in seconds. """
+    def split_audio(self, audio_file: AudioFile, segment_time: int = 180, output_dir: str = "/tmp") -> bool:
+        """Splits an audio track at intervals determined by the supplied segment time in seconds."""
         command = [
-            'ffmpeg',
-            '-y',
-            '-i', audio_file.absolute_path,
-            '-f', 'segment',
-            '-segment_time', f'{segment_time}',
-            '-c', 'copy',
-            f'{output_dir}/{audio_file.title}%03d.mp3',
+            "ffmpeg",
+            "-y",
+            "-i",
+            audio_file.absolute_path,
+            "-f",
+            "segment",
+            "-segment_time",
+            f"{segment_time}",
+            "-c",
+            "copy",
+            f"{output_dir}/{audio_file.title}%03d.mp3",
         ]
 
         ff = FfmpegProgress(command)
@@ -61,9 +65,9 @@ class AudioHandler:
             )
 
     def tag_mp3(self, audio_file: AudioFile, artist_name: str, album_name: str) -> None:
-        """ Tags mp3 file with the supplied metadata. """
+        """Tags mp3 file with the supplied metadata."""
         audio = EasyID3(audio_file.absolute_path)
-        audio['title'] = audio_file.title
-        audio['artist'] = artist_name
-        audio['album'] = album_name
+        audio["title"] = audio_file.title
+        audio["artist"] = artist_name
+        audio["album"] = album_name
         audio.save()
